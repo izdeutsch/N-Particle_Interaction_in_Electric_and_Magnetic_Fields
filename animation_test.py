32c71542165633a4c335ca48268c686cfecc3581
Isabella Deutsch <isabelladeutsch8@berkeley.edu>
2024-07-30 21:57:28 -0500

    animation_test different example, note to talk to Amin/Krish about velocities/mass

--- animation_test.py
+++ animation_test.py
@@ -21,7 +21,7 @@ def simple_test2():
     charge = np.asarray([1, -1])
     mass = np.asarray([1, 2])
     p = Particles(positions, charge, mass)
-    c1 = camera((0, 0, 0), (0, 0, 1), (1, 0, 0), p)
+    c1 = camera((0, 0, 0), (0, 0, 1), (1, 0, 0), p) # point the camera and then set orientation
     c2 = camera((0, 0, 0), (-1, 0, 0), (0, 1, 0), p)
     while(c1.alive and c2.alive):
         c1.render()
@@ -31,10 +31,11 @@ def simple_test2():
 def animation_test():
     positions = np.asarray([[0, 0, 0], [0, 0, 200]])
     charge = np.asarray([1, -1])
-    mass = np.asarray([100000, 1])
+    mass = np.asarray([100000, 1000]) 
+    '''Isabella: note how mass has to be extremely large and similar to work, discuss with Krish/Amin velocities'''
     p = Particles(positions, charge, mass)
     get_pos_vel = get_function(p, get_fields2)
-    y0 = [0, 0, 0, 0, 0, 0, 0, 0, 200, 0, -3403.54263, 0]  # pos1, vel1, pos2, vel2
+    y0 = [0, 0, 0, 0, 0, 0, 0, 0, 200, 0, -271.65, 0]  # pos1, vel1, pos2, vel2
     answer = solve_ivp(get_pos_vel, y0=y0, method='RK45', t_span=[0, 1], rtol=1e-4,
                        args=(lambda x, y, z: (0, 0, 0, 0, 0, 0),))
     c = camera((0, 0, 0), (-1, 0, 0), (0, 1, 0), p, width= 1000, height= 700)
@@ -48,7 +49,7 @@ def animation_test():
         if(not c.alive):
             break
         c.render()
-        time.sleep(t[i])
+        time.sleep(t[i]/2) # speeding up the time between renderings
         c.update_particles([(y[0][i], y[1][i], y[2][i]), (y[6][i], y[7][i], y[8][i])])
 
 #simple_test1()
